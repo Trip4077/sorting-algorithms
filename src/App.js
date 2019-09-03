e75f5df3887885f782dbb6466e8d85d474495954
Trip1701 <bj2336@email.vccs.edu>
2019-09-02 22:15:41 -0400

    Base ControlPanel Component Created

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import Header from './components/Header';
+import ControlPanel from './components/ControlPanel';
 
 class App extends React.Component {
   constructor(props) {
@@ -16,6 +17,7 @@ class App extends React.Component {
       <div className="App">
         Hello World
         <Header />
+        <ControlPanel algorithm={this.state.algorithm} />
       </div>
     );
   }
---
+++ src/components/ControlPanel.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ControlPanel = props => {
+    return(
+        <div>
+            Control Panel
+            <p>{props.algorithm}</p>
+        </div>
+    )
+}
+
+export default ControlPanel;
