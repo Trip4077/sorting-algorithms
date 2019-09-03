ae1421493eac141fa9d2077fa6a2c6db49a123ed
Trip1701 <bj2336@email.vccs.edu>
2019-09-02 23:44:24 -0400

    Merge Sort Functional

--- src/algorithms.js
+++ src/algorithms.js
@@ -1,31 +1,23 @@
+import { EFAULT } from "constants";
+
 const Aglorithms = {
-    merge: arr => {
-        console.log('merge');
-        console.log(arr)
+    merge: array => {
+        return merge(array);
     },
 
-    insertion: array => {
+    insertion: arr => {
         console.log('insertion')
-        for(let i = 1; i < array.length; i++) {
-            //Loop through entire array for comparison
-            //console.log('outer', array[i]);
+        for(let i = 1; i < arr.length; i++) {
             for(let j = 0; j < i; j++) {
-              //check if value is less than current index
-              //console.log('inner', array[j]);
-              if(array[i] < array[j]) {
-        
-                //if so, store in temp variable removing it from array
-                //console.log('pre-sort', array);
-                const temp = array.splice(i, 1);
-                //console.log('temp', temp);
-                //INSERT value into correct index
-                array.splice(j, 0, temp[0]);
-                //console.log('post-sort', array);
-              }
-            }
-          }
+                if(arr[i] < arr[j]) {
+                    const temp = arr.splice(i, 1);
 
-        return array;
+                    arr.splice(j, 0, temp[0]);
+                }
+            }
+        }
+
+        return arr;
     },
 
     bubble: arr => {
@@ -35,6 +27,37 @@ const Aglorithms = {
     quick: arr => {
         console.log('quick');
     }
+}
+
+const mergeParts = (left, right) => {
+    const result = [];
+
+    while(left.length && right.length) {
+        if(left[0] <= right[0]) {
+            result.push(left.shift());
+        } else {
+            result.push(right.shift());
+        }
+    }
+
+    while(left.length) result.push(left.shift());
+
+    while(right.length) result.push(right.shift());
+
+    return result;
+}
+
+const merge = array => {
+    console.log('merge');
+    
+    if(array.length < 2) return array;
+
+    const middle = Math.floor(array.length / 2);
+
+    const left = array.slice(0, middle);
+    const right = array.slice(middle, array.length);
+
+    return mergeParts(merge(left), merge(right));
 }
 
 export default Aglorithms;
